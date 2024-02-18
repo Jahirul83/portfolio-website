4d9a7c189125520267942bb8132fca8bd6814a92
Jahirul83 <jahirulislamcse83@gmail.com>
2024-02-19 01:48:56 +0600

    Project page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,7 +2,7 @@
 
 const About = () => {
     return (
-        <div className="w-full py-10 bg-gradient-to-b from-gray-800 to-black">
+        <div className="w-full py-10 bg-gradient-to-b from-gray-800 to-black text-white">
             <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">About Me</p>
@@ -11,7 +11,7 @@ const About = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas assumenda voluptatem provident placeat neque commodi ratione harum delectus, eius voluptatum ipsum animi at in vitae! Voluptatum hic animi illum soluta expedita, quibusdam sunt aperiam veniam facere totam tempore placeat accusantium itaque similique eveniet omnis doloribus ipsa molestias numquam ducimus. Dolorem.
                 </p>
                 <br />
-                <p className="text-xl"> 
+                <p className="text-xl">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, reiciendis aspernatur! Sapiente ullam assumenda in, esse dolorum tenetur rerum voluptas ipsa aliquid eum sit reiciendis cumque nobis. Praesentium quidem eligendi cumque possimus modi adipisci ut perspiciatis ab, ea quo culpa assumenda nesciunt deleniti perferendis voluptates autem laboriosam libero numquam provident?
                 </p>
             </div>
